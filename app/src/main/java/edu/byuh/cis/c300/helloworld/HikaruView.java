a4b2bdc635ad00fc429a412d72f0827a3bf67198
thirstynellan <gmd2@byuh.edu>
2024-09-16 09:50:15 -1000

    draw and scale duck

--- app/src/main/java/edu/byuh/cis/c300/helloworld/HikaruView.java
+++ app/src/main/java/edu/byuh/cis/c300/helloworld/HikaruView.java
@@ -1,6 +1,8 @@
 package edu.byuh.cis.c300.helloworld;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,14 +11,18 @@ import android.view.View;
 public class HikaruView extends View {
 
     private Paint grace;
+    private Bitmap duckImg;
+    private boolean initialized;
 
     public HikaruView(Context k) {
         super(k);
+        initialized = false;
         grace = new Paint();
         grace.setColor(Color.rgb(255,200,200));
         grace.setStyle(Paint.Style.STROKE);
         grace.setTextSize(100);
         grace.setTextAlign(Paint.Align.CENTER);
+        duckImg = BitmapFactory.decodeResource(getResources(), R.drawable.duck);
     }
 
     @Override
@@ -28,9 +34,15 @@ public class HikaruView extends View {
         float rectRight = w * 0.75f;
         float rectTop = h * 0.2f;
         float rectBottom = h * 0.4f;
-        grace.setStrokeWidth(w * 0.01f);
+        if (!initialized) {
+            int duckSize = (int) (w * 0.25);
+            duckImg = Bitmap.createScaledBitmap(duckImg, duckSize, duckSize, true);
+            grace.setStrokeWidth(w * 0.01f);
+            initialized = true;
+        }
         c.drawRect(rectLeft, rectTop, rectRight, rectBottom, grace);
         c.drawLine(w*0.5f, h*0.3f, w*0.8f, h*0.9f, grace);
         c.drawText("Hello CS300", w*0.5f, h*0.5f, grace);
+        c.drawBitmap(duckImg, w*0.4f, h*0.6f, grace);
     }
 }
