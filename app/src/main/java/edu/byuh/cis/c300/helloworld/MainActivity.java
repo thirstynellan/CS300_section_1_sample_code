1bd83be313835223c17b7c9b59d7a7a8ad24a2b8
thirstynellan <gmd2@byuh.edu>
2024-09-20 09:59:17 -1000

    made a duck object!

--- app/src/main/java/edu/byuh/cis/c300/helloworld/MainActivity.java
+++ app/src/main/java/edu/byuh/cis/c300/helloworld/MainActivity.java
@@ -2,11 +2,9 @@ package edu.byuh.cis.c300.helloworld;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import edu.byuh.cis.c300.helloworld.ui.HikaruView;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/edu/byuh/cis/c300/helloworld/ui/HikaruView.java
+++ app/src/main/java/edu/byuh/cis/c300/helloworld/ui/HikaruView.java
@@ -1,17 +1,18 @@
-package edu.byuh.cis.c300.helloworld;
+package edu.byuh.cis.c300.helloworld.ui;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
+import edu.byuh.cis.c300.helloworld.sprites.Duck;
+
 public class HikaruView extends View {
 
     private Paint grace;
-    private Bitmap duckImg;
+    //private Bitmap duckImg;
+    private Duck kanaan;
     private boolean initialized;
 
     public HikaruView(Context k) {
@@ -22,7 +23,7 @@ public class HikaruView extends View {
         grace.setStyle(Paint.Style.STROKE);
         grace.setTextSize(100);
         grace.setTextAlign(Paint.Align.CENTER);
-        duckImg = BitmapFactory.decodeResource(getResources(), R.drawable.duck);
+        //duckImg = BitmapFactory.decodeResource(getResources(), R.drawable.duck);
     }
 
     @Override
@@ -35,14 +36,15 @@ public class HikaruView extends View {
         float rectTop = h * 0.2f;
         float rectBottom = h * 0.4f;
         if (!initialized) {
-            int duckSize = (int) (w * 0.25);
-            duckImg = Bitmap.createScaledBitmap(duckImg, duckSize, duckSize, true);
+            kanaan = new Duck(getResources(), w);
+            kanaan.setLocation(w*0.4f, h*0.6f);
             grace.setStrokeWidth(w * 0.01f);
             initialized = true;
         }
         c.drawRect(rectLeft, rectTop, rectRight, rectBottom, grace);
         c.drawLine(w*0.5f, h*0.3f, w*0.8f, h*0.9f, grace);
         c.drawText("Hello CS300", w*0.5f, h*0.5f, grace);
-        c.drawBitmap(duckImg, w*0.4f, h*0.6f, grace);
+        //c.drawBitmap(duckImg, w*0.4f, h*0.6f, grace);
+        kanaan.draw(c);
     }
 }
